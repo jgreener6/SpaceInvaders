c1d7c7970869796883cb5d3cfcb6bae624456b7a
jgreener6 <justinmgreen06@gmail.com>
2023-02-22 21:03:11 -0500

    JS changes

--- spaceInvaders.js
+++ spaceInvaders.js
@@ -1,7 +1,17 @@
 (function (){
+var theImg = document.querySelector('#theImg');
+var container = document.querySelector('#spaceShip');
+
     const spaceShip = document.getElementById('spaceShip')
 
-    spaceShip.addEventListener("keypress", event) => {}
+    container.addEventListener("keypress", getClickPosition, false);
+
+    function getClickPosition(e){
+
+        theImg.style.left = 300 +"px";
+        theImg.style.top = 300 + "px";
+        
+    }
     
 
  }());
